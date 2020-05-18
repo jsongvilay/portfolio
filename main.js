eeb3fdfea55c3017d080c379822eaf657842d968
jsongvilay <57165394+jsongvilay@users.noreply.github.com>
2020-05-17 23:32:36 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,35 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("sub-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var subpanel = this.nextElementSibling;
+    if (subpanel.style.maxHeight) {
+      subpanel.style.maxHeight = null;
+    } else {
+      subpanel.style.maxHeight = subpanel.scrollHeight + "px";
+    }
+  });
+}
+
+AOS.init({
+  easing: linear,
+  delay: 300,
+  duration: 1200,
+})
